05f2e5520a146967ce7b2d3b9e2475b7000f62ec
Jennifer-Zheng <jenn.zheng@yahoo.com>
2020-05-15 15:40:58 -0500

    Logic is implemented. Need to fix timing issue with testbench.

--- instruction_queue.v
+++ instruction_queue.v
@@ -34,7 +34,7 @@ output reg stall_out
 );
 
 // Instruction queue: 76 bits wide * 8 entries
-reg [75:0] queue [7:0];
+reg [74:0] queue [7:0];
 
 // Location of next instruction to dispatch (aka start of queue)
 integer start_idx = 0;
@@ -45,31 +45,77 @@ integer end_idx = 0;
 // Number of instructions that are currently in the queue
 integer counter = 0;
 
-// Holds concatenation of the instruction metadata
-reg [75:0] instruction;
+// Holds concatenation of the instruction metadata that was fetched
+reg [74:0] instruction_fetched;
 
-always @(posedge clk) begin
-    // Queue is not yet at capacity so we can continue
+// Instruction to be dispatched
+reg [74:0] instruction_dispatched;
+
+always @(negedge clk) begin 
+    if(stall_in == 1 && counter == 8) begin
+        stall_out = stall_in;
+    end else begin
+        stall_out = 0;
+    end
+end
+
+integer test = 0;
+always @(posedge clk) begin 
+    if(test < 5) begin
+    // Dispatch handling code (only when dispatch is not stalled and queue is not empty)
+    if (stall_in == 0 && counter > 0) begin
+        instruction_dispatched[74:0] = queue[start_idx];
+        $display("instruction_dispatched: %b", instruction_dispatched);
+        MajorOpcode_out[3:0] = instruction_dispatched[74:71];
+        $display("MajorOpcode_out: %b", MajorOpcode_out);
+        Source1_out[4:0] = instruction_dispatched[70:66];
+        $display("Source1_out: %b", Source1_out);
+        Source2_out[4:0] = instruction_dispatched[65:61];
+        $display("Source2_out: %b", Source2_out);
+        OffsetScale_out[1:0] = instruction_dispatched[60:59];
+        $display("OffsetScale_out: %b", OffsetScale_out);
+        Destination_out[4:0] = instruction_dispatched[58:54];
+        $display("Destination_out: %b", Destination_out);
+        MinorOpcode_out[3:0] = instruction_dispatched[53:50];
+        $display("MinorOpcode_out: %b", MinorOpcode_out);
+        HasAddress_out = instruction_dispatched[49];
+        $display("HasAddress_out: %b", HasAddress_out);
+        Address_out[47:0] = instruction_dispatched[48:1];
+        $display("Address_out: %b", Address_out);
+        OffsetSub_out = instruction_dispatched[0];
+        $display("OffsetSub_out: %b", OffsetSub_out);
+
+        // Reset start_idx when we reach the end of the queue
+        if(start_idx == 7) begin
+            start_idx = 0;
+        end else begin
+            start_idx = start_idx + 1;
+        end
+        $display("start_idx: %d", start_idx);
+
+        counter = counter - 1;
+        $display("counter: %d", counter);
+    end
+
+    // Handle fetched instruction (only when we are not at capacity)
     if(counter != 8) begin
-        stall_out <= stall_in;
-        instruction[75:0]  <= {MajorOpcode_in, Source1_in, Source2_in, OffsetScale_in, Destination_in, MinorOpcode_in, HasAddress_in, Address_in, OffsetSub_in, stall_in};
-        $display("instruction bits: %b", instruction);
-        queue[end_idx] <= instruction;
+        instruction_fetched[74:0]  = {MajorOpcode_in, Source1_in, Source2_in, OffsetScale_in, Destination_in, MinorOpcode_in, HasAddress_in, Address_in, OffsetSub_in};
+        $display("instruction_fetched: %b", instruction_fetched);
+
+        queue[end_idx] = instruction_fetched;
         $display("queue info: %b", queue[end_idx]);
-        end_idx = end_idx + 1;
-        $display("end_idx: %d", end_idx);
         counter = counter + 1;
         $display("counter: %d", counter);
 
         // Reset end_idx when we reach the end of the queue
         if(end_idx == 7) begin
             end_idx = 0;
+        end else begin
+            end_idx = end_idx + 1;
         end
-    
+        $display("end_idx: %d", end_idx);
     end 
-    else begin
-        // Queue is at capacity so stall until we have an empty slot.
-
-    end 
+    test = test + 1;
+    end
 end
 endmodule /* main */
--- iq_testbench.v
+++ iq_testbench.v
@@ -36,20 +36,24 @@ integer test_num = 0;
 instruction_queue q(r_CLOCK, MajorOpcode_in, Source1_in, Source2_in, OffsetScale_in, Destination_in, MinorOpcode_in, HasAddress_in, Address_in, OffsetSub_in, stall_in,
 MajorOpcode_out, Source1_out, Source2_out, OffsetScale_out, Destination_out, MinorOpcode_out, HasAddress_out, Address_out, OffsetSub_out, stall_out);
 
-always @(posedge r_CLOCK) begin
-    if (test_num == 0) begin
+initial begin
+    $display("IQ Testbench start");
+end
+
+always @(negedge r_CLOCK) begin
+    // if (test_num == 0) begin
         // $display("HERE");
-        MajorOpcode_in <= 4'b1010;
-        Source1_in <= 5'b11111;
-        Source2_in <= 5'b01110;
-        OffsetScale_in <= 2'b11;
-        Destination_in <= 5'b11011;
-        MinorOpcode_in <= 4'b1001;
-        HasAddress_in <= 1;
-        Address_in <= 98;
-        OffsetSub_in <= 1;
-        stall_in <= 1;
-    end
+        MajorOpcode_in = 4'b1010;
+        Source1_in = 5'b11111;
+        Source2_in = 5'b01110;
+        OffsetScale_in = 2'b11;
+        Destination_in = 5'b11011;
+        MinorOpcode_in = 4'b1001;
+        HasAddress_in = 1;
+        Address_in = 98;
+        OffsetSub_in = 1;
+        stall_in = 0;
+    // end
 end
 
 
